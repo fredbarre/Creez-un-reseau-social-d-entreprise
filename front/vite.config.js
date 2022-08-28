45dc9baf107bc9926198b0541e8c94d3bb9ae7a6
fredbarre <fredbarremail@protonmail.com>
2022-08-28 14:10:56 +0200

    installation vite-plugin-mix morgan express sass

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mix from "vite-plugin-mix";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), mix.default({ handler: "./src/api/index.js" })],
+});
