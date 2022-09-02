440bcdfaa881b05823c4a718dc64fe8019923a2e
fredbarre <fredbarremail@protonmail.com>
2022-09-02 02:57:00 +0200

    Creation PostsPage

---
+++ front/src/components/AllPosts.jsx
@@ -0,0 +1,83 @@
+function AllPosts() {
+    return (
+        <section>
+        <div class="box column is-three-fifths is-offset-one-fifth">
+            <article class="media">
+                <div class="media-left">
+                    <figure class="image is-64x64">
+                        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"/>
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>John Smith</strong>
+                            <br/>
+                            Titre
+                            <br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa
+                            fringilla
+                            egestas. Nullam condimentum luctus turpis.azeazeazeazezajeaozje ajzpe jazpo ejaz eojza
+
+
+                            azoepakezzepazea
+                            eakzelazkeazkezaùeza
+
+                            ezapekzlaezklzekaz
+                        </p>
+                    </div>
+                    <nav class="level is-mobile">
+                        <div class="level-left">
+                            <a class="level-item" aria-label="reply">
+                                Details
+                            </a>
+
+                            <a class="level-item" aria-label="like">
+                                <span class="icon is-small">
+                                    <i class="fas fa-heart" aria-hidden="true"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        </div>
+
+        <div class="box column is-three-fifths is-offset-one-fifth">
+            <article class="media">
+                <div class="media-left">
+                    <figure class="image is-64x64">
+                        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>John Smith</strong>
+                            <br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa
+                            fringilla
+                            egestas. Nullam condimentum luctus turpis.
+                        </p>
+                    </div>
+                   <nav class="level is-mobile">
+                        <div class="level-left">
+                            <a class="level-item" aria-label="reply">
+                                Details
+                            </a>
+
+                            <a class="level-item" aria-label="like">
+                                <span class="icon is-small">
+                                    <i class="fas fa-heart" aria-hidden="true"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        </div>
+    </section>
+    )
+}
+
+export default AllPosts;
---
+++ front/src/pages/PostsPage.jsx
@@ -0,0 +1,12 @@
+import AllPosts from "../components/AllPosts";
+import Header from "../components/Header";
+
+function PostsPage() {
+    return (
+        <div>
+      <Header/><AllPosts />
+      </div>
+    )
+}
+
+export default PostsPage;
