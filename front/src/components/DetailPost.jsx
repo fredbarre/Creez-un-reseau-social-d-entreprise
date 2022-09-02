efef4f464cacd610f73c1bb8e2b3227fbf8991b3
fredbarre <fredbarremail@protonmail.com>
2022-09-02 02:51:11 +0200

    creation de postPage

---
+++ front/src/components/DetailPost.jsx
@@ -0,0 +1,28 @@
+function DetailPost() {
+    return (
+        <div>
+    <br />
+    <img src="./src/assets/imagesposts/nuages.jpg" width="256" height="256" class="column is-half is-offset-one-quarter"/>
+    <div class="block box">
+        Auteur <strong>block</strong>.
+    </div>
+    <div class="block box">
+        Titre du post
+    </div>
+    <div class="block box">
+        Text du post
+        <br/>
+        azeazeazeazezajeaozjee
+        az
+        eakzelazkeazkezaùezaazeaz
+
+        zaeazeeaze
+        eakzelazkeazkezaùezaazeaz
+
+        eakzelazkeazkezaùeza
+                </div>
+                </div>
+    )
+}
+
+export default DetailPost;
---
+++ front/src/pages/PostPage.jsx
@@ -0,0 +1,12 @@
+import DetailPost from "../components/DetailPost";
+import Header from "../components/Header";
+
+function PostPage() {
+    return (
+        <div>
+      <Header/><DetailPost />
+      </div>
+    )
+}
+
+export default PostPage;
