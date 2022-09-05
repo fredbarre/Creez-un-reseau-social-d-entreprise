438e7dc6cd47e932762c9aa261338b0bcbad6e26
fredbarre <fredbarremail@protonmail.com>
2022-09-05 11:38:36 +0200

    création du router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import LoginPage from './pages/LoginPage'
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import NewPostPage from "./pages/NewPostPage";
+import PostPage from "./pages/postPage";
+import PostsPage from "./pages/PostsPage";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <LoginPage/>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/newPost" element={<NewPostPage />} />
+        <Route path="/post" element={<PostPage />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/myposts" element={<PostsPage />} />
+        <Route path="/settings" element={<PostsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,55 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 //import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import App from './App'
-import './index.scss'
-import './App.css'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage';
-import NewPostPage from './pages/NewPostPage';
-import PostPage from './pages/postPage';
-import PostsPage from './pages/PostsPage';
+import "./index.scss";
+import "./App.css";
+import App from "./App";
 
-/*
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Route path="/login">
-        <LoginPage/>
-      </Route>
-    </Router>
-    <App />
-  </React.StrictMode>
-)
-*/
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Route path="/login">
-        <LoginPage />
-      </Route>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-*/
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />}>
-          <Route path="/sigup" element={<SignupPage />} />
-          <Route path="/newPost" element={<NewPostPage />} />
-          <Route path="/post" element={<PostPage />} />
-          <Route path="/posts" element={<PostsPage />} />
-         
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
