4ee26adce36e77be10f9265440b9a4dd794600da
fredbarre <fredbarremail@protonmail.com>
2022-09-05 11:37:52 +0200

    utilisation de Link de react-router-dom

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,68 +1,79 @@
-import GroupomaniaLogo from '../assets/icon-left-font.svg';
-import SignupForm from './SignupForm';
+import GroupomaniaLogo from "../assets/icon-left-font.svg";
+import SignupForm from "./SignupForm";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function PageInscription() {
-    ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Header /><SignupForm />
-  </React.StrictMode>
-)
-
-}
 function Header() {
-    return (
+  const [active, setActive] = useState(false);
+
+  return (
     <header>
-            <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-                <img src="./src/assets/icon-left-fontadjusted.png" width="112" height="28"/>
-            </a>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <img
+              src="./src/assets/icon-left-fontadjusted.png"
+              width="112"
+              height="28"
+            />
+          </Link>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-                data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
+          <a
+            role="button"
+            className={`navbar-burger ${active ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={() => setActive(!active)}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
 
-        <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item">
-                    Mon profil
-                </a>
-                <a class="navbar-item">
-                    Tous les posts
-                </a>
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${active ? "is-active" : ""}`}
+        >
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/settings">
+              Mon profil
+            </Link>
+            <Link className="navbar-item" to="/posts">
+              Tous les posts
+            </Link>
 
-                <a class="navbar-item">
-                    Mes posts
-                </a>
+            <Link className="navbar-item" to="/myposts">
+              Mes posts
+            </Link>
 
-                <a class="navbar-item">
-                    Nouveau post
-                </a>
+            <Link className="navbar-item" to="/login">
+              Nouveau post{" "}
+            </Link>
+          </div>
 
-
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <Link className="button is-primary" to="/login">
+                  <strong>Deconnexion</strong>
+                </Link>
+                <Link className="button is-primary" to="/signup">
+                  <strong>Inscription</strong>
+                </Link>
+                <Link className="button is-light" to="/login">
+                  Connexion
+                </Link>
+              </div>
             </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button is-primary">
-                            <strong>Deconnexion</strong>
-                        </a>
-
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    </nav>
+      </nav>
 
-    <br />
-    
+      <br />
     </header>
-    );
+  );
 }
 
 export default Header;
