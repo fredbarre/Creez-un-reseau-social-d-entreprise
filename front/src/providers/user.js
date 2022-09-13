94ce00d0cd8be9ace5b735c8d301650cfe38806f
fredbarre <fredbarremail@protonmail.com>
2022-09-13 12:20:28 +0200

    création provider user

---
+++ front/src/providers/user.js
@@ -0,0 +1,19 @@
+async function fetchsignup(params) {
+  const response = await fetch(`http://localhost:3000/api/auth/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  });
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const responseData = await response.json();
+  //console.log(responseData);
+  return responseData;
+}
